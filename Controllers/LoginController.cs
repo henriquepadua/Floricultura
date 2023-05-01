52ddef8cc0915393a95d117c908c0c482c1ddce8
henriquepadua <henrique.lommes@gmail.com>
2023-04-30 23:34:18 -0300

    adptando projeto para padrao mvc

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Floricultura.Controllers
+{
+    public class LoginController : Controller
+    {
+        [AcceptVerbs("GET", "POST")]
+        [Route("Login")]
+        public async Task<IActionResult> Index()
+        {
+               return View();
+        }
+    }
+}
